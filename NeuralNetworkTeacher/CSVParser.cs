a0a586532ff1b9c6b4a10923fa54db2a50e8c84c
promo\Acer <azpk360@yandex.ru>
2017-12-09 16:07:08 +0500

    Добавлен парсер таблицы с данными для обучения и модуль VK.

---
+++ NeuralNetworkTeacher/CSVParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NeuralNetworkTeacher
+{
+    public class CSVParser
+    {
+        public Dictionary<string, IList<string>> Parse(string csvFilePath)
+        {
+            var result = new Dictionary<string, IList<string>>();
+            using (var streamReader = new StreamReader(csvFilePath, Encoding.GetEncoding(1251)))
+            {
+                // Первая строка содержит формат и не нужна нам.
+                var line = streamReader.ReadLine();
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    var operatedRow = CSVRowReader(line);
+                    if (!result.ContainsKey(operatedRow.Item1))
+                        result.Add(operatedRow.Item1, new List<string>{operatedRow.Item2});
+                    else
+                        result[operatedRow.Item1].Add(operatedRow.Item2);
+                }
+            }
+            return result;
+        }
+
+        private (string, string) CSVRowReader(string tableRow)
+        {
+            var row = tableRow.Split(';').ToList();
+            var category = row[row.Count - 1].Replace(".txt", "");
+            row.RemoveAt(row.Count - 1);
+            
+            return (category, string.Join("", row));
+        }
+    }
+}
---
+++ NeuralNetworkTeacher/Tests/CSVParserShould.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace NeuralNetworkTeacher.Tests
+{
+    [TestFixture]
+    public class CSVParserShould
+    {
+        private CSVParser Parser;
+        [SetUp]
+        public void SetUp()
+        {
+            Parser = new CSVParser();
+        }
+        
+        [Test]
+        public void GiveCorrectParsedData()
+        {
+            var expected = new Dictionary<string, IList<string>>
+            {
+                {"aaaa", new List<string>{"aaa;bbb", "aaa bbb", "cccddd"}},
+                {"bbbb", new List<string>{"aaa;bbb1", "aaa bbb1", "cccddd1"}},
+                {"cccc", new List<string>{"aaa;bbb2", "aaa bbb2", "cccddd2"}}
+            };
+            var path = Path.Combine("...test_interest.csv");
+            Parser.Parse(path).Should().BeEquivalentTo(expected);
+        }
+    }
+}
